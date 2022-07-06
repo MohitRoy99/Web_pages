3cb39d645c3d61f418ef7d585d68b2d1e3e0e4ba
Aritra Basak <basakaritra10@gmail.com>
2022-07-06 15:01:37 +0530

    Delete Gym_Page directory

--- Gym_Page/navbar.js
+++
@@ -1,8 +0,0 @@
-const nav = document.getElementById('mid') ;
-
-
-const hamIcon = () =>{
-   nav.classList.toggle('active') ;
-   const icon = document.getElementById('hamburger-icon');
-   icon.textContent = icon.textContent == "menu" ? "menu_open" : "menu";
-}
